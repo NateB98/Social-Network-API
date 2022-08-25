96602474deb30393ed086c6a6e9cf900d4a5a6f9
NateB <nathanbrengman@gmail.com>
2022-08-24 20:05:38 -0500

    added more files and added codeto routes and controllers

---
+++ controllers/users-controller.js
@@ -0,0 +1,37 @@
+const { Users } = require('../models')
+
+const userController = {
+    createUsers({body}, res) {
+        Users.create(body)
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => res.status(400).json(err));
+    },
+    getAllUsers(req, res) {
+    Users.find({})
+    .populate({path: 'friends', select: '-__v'})
+    .populate({path: 'thoughts', select: '-__v'})
+    .select('-__v').then(dbUsersData => res.json(dbUsersData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    },
+    getUsersById({params}, res) {
+        Users.findOne({_id: params.id })
+        .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        // return if no user is found 
+        .then(dbUsersData => {
+            if(!dbUsersData) {
+                res.status(404).json({message: 'There is no user with this ID'});
+                return;
+            }
+            res.json(dbUsersData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        })
+    },
+}
